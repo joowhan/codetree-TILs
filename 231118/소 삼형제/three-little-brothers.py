3d43776bef91d6b40c6781eecf350b5495a772fc
Joowhan <70326417+joowhan@users.noreply.github.com>
2023-11-18 13:44:41 +0900

    Title: 소 삼형제, Time: 135ms, Memory: 25MB, Status: Passed - CodeTree

--- 231118/소 삼형제/three-little-brothers.py
+++ 231118/소 삼형제/three-little-brothers.py
@@ -1,8 +1,12 @@
 n = int(input())
-temp = []
+temp = {}
 for i in range(n):
     cows = list(input().split())
     cows.sort()
-    if cows not in temp:
-        temp.append(cows)
-print(len(temp))
+    cows = tuple(cows)
+    if cows in temp.keys():
+        temp[cows] += 1
+    else:
+        temp[cows] = 1
+answer = sorted(temp.items(), key=lambda x:x[1], reverse=True)
+print(answer[0][1])
